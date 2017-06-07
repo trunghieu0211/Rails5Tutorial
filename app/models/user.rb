9edcab7406cb13bc947d7629ee17dbc4a063368b
TrungHieu <harrypotter12g@gmail.com>
2017-06-07 15:55:14 +0700

    Chap7 Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length:{ maximum: Settings.user.max_name_size }
+  before_save {email.downcase!}
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length:{ maximum: Settings.user.max_email_size }, format:{ with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
+  validates :name, presence: true, length: {maximum: Settings.user.max_name_size}
+  validates :email, presence: true, length: {maximum: Settings.user.max_email_size},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.user.min_password_size}
+
   has_secure_password
-  validates :password, presence: true, length:{ minimum: Settings.user.min_password_size }
 end
